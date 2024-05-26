7400c163d16b97c684cb94e42c2197d5d559ab19
kwasi <affulnana04@gmail.com>
2024-05-26 23:47:03 +0000

    Made the name bold

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style= {styles.text}>My name is Nana Kwasi</Text>
+      <Text style= {styles.text}>
+        My name is <Text style= {styles.boldText}>Nana Kwasi</Text>
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,11 +15,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#dfa',
+    backgroundColor: '#fda',
     alignItems: 'center',
     justifyContent: 'center',
   },
   text: {
     fontSize: 24,
+  },
+  boldText: {
+    fontWeight: 'bold',
   }
 });
